0e714cae44a733fa0aafb55a842a980b2ca141fb
Sandro Sorte <sandrosorte@gmail.com>
2023-05-29 11:01:12 -0300

    inicialização do projeto loading, construção do layout

---
+++ loading.js
@@ -0,0 +1,20 @@
+const btn = document.querySelector('.btn')
+const btn1 = document.querySelector('.btn1')
+const load = [...document.querySelectorAll('.ld1')]
+let clicado = false;
+
+btn.addEventListener('click',()=>{
+    btn.classList.add('paused')
+    btn1.style.display= 'block'
+    clicado = true
+    load.map((e)=>{
+        e.classList.add('run')
+    })
+})
+btn1.addEventListener('click',()=>{
+    load.map((e)=>{
+        e.classList.remove('run')
+        btn.classList.remove('paused')
+        btn1.style.display= 'none'
+    })
+})
